c211279b0acc2c9b6250b1f969cf58932a0d932b
Caia Chuang <caiachuang@gmail.com>
2021-05-10 14:31:43 -0700

    Connect with database.

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-import express from 'express';
-
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+const connectionURL =
+    "mongodb+srv://admin:BOQd4Im50aCbRufc@cluster0.7epvq.mongodb.net/whatsappDb?retryWrites=true&w=majority";
+mongoose.connect(connectionURL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-app.get('/', (req, res) => res.status(200).send("YO WHAT UP"));
+app.get("/", (req, res) => res.status(200).send("YO WHAT UP"));
 
 app.listen(port, () => {
-    console.log(`server is listening on port: ${port}`)
+    console.log(`server is listening on port: ${port}`);
 });
