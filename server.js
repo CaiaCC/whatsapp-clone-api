c3ad7afdd725189fbd7ef36ac518ecef2ad9f97a
Caia Chuang <caiachuang@gmail.com>
2021-05-10 15:48:03 -0700

    Add Pusher Trigger.

--- server.js
+++ server.js
@@ -32,7 +32,21 @@ db.once('open', () => {
     const changeStream = msgCollection.watch();
 
     changeStream.on('change', change => {
-        console.log("A change occured: ", change)
+        console.log("A change occurred: ", change);
+
+        if (change.operationType === 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger(
+                'messages',
+                'inserted',
+                {
+                    name: messageDetails.user,
+                    message: messageDetails.message
+                }
+            );
+        } else {
+            console.log("Error triggering Pusher");
+        }
     })
 })
 
