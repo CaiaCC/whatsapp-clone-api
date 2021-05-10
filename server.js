f7170a6ea98e63b43ad6a9e520f9c92c9bad2e4d
Caia Chuang <caiachuang@gmail.com>
2021-05-10 16:09:27 -0700

    Add timestamp field sending to frontend.

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ db.once("open", () => {
             pusher.trigger("messages", "inserted", {
                 name: messageDetails.name,
                 message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
             });
         } else {
             console.log("Error triggering Pusher");
